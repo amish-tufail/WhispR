72000fbab9310c6eadf2a42b075e6a75bc36b1a4
Amish Tufail <amish.tufail2002@gmail.com>
2023-07-15 13:05:57 +0300

    Initial Commit

---
+++ WhispR/WhispRApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhispRApp.swift
+//  WhispR
+//
+//  Created by Amish on 15/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct WhispRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
